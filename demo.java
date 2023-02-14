80b018b97a3921f5dd8a9785f96c47470ad9b53a
likitarai2001 <likita.r@simformsolutions.com>
2023-02-14 14:50:31 +0530

    remove println

--- demo.java
+++ demo.java
@@ -4,7 +4,6 @@ import java.io.*;
 public class HelloWorld{
     public static void main(String args[]){
         System.out.println("Hello World");
-        System.out.println("Java Program");
     }
 
 }
